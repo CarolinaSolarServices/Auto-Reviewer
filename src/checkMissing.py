0041e1e141c71c460ddd47c03461c8a5e9ae8700
Lizzy Zhou <zhongle@unc.edu>
2023-10-04 11:52:13 -0400

    fixed subset

--- src/checkMissing.py
+++ src/checkMissing.py
@@ -155,13 +155,13 @@ def check_and_autofill_inverter_and_voltage(df):
         if column_count > 12:
             subset = pd.concat(
                 [
-                    df[important_condition].iloc[:, :9],
-                    df[important_condition].iloc[:, -3:],
+                    df[important_condition & fill_condition].iloc[:, :9],
+                    df[important_condition & fill_condition].iloc[:, -3:],
                 ],
                 axis=1,
             )
         else:
-            subset = df[important_condition]
+            subset = df[important_condition & fill_condition]
         log(f"Here are filled records within the daytime.\n" f"{get_info( subset)}")
 
     important_still_missing = df[
