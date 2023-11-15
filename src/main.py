50c44a09eb6a3082616f9617245a3f963cb6dce9
Lizzy Zhou <zhongle@unc.edu>
2023-11-14 20:33:12 -0500

    reorder summary table

--- src/main.py
+++ src/main.py
@@ -19,6 +19,7 @@ def main(directory="../data"):
             print(csv_file.name)
             process_file(csv_file.path)
             getInfo.log_messages = []
+    Summary.summary = Summary.summary.sort_values(by="Site Name")
     Summary.summary.to_csv(f"../output/summary.csv", index=False)
 
 
